eca156efe3838e0bd2de80ab1f985f80d09b1b84
Jean Mayard <sanctuarygames30@gmail.com>
2023-01-08 21:05:07 -0500

    symbol to find data. Update shows functinonality

--- src/pages/price.js
+++ src/pages/price.js
@@ -1,17 +1,26 @@
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 import  data  from "../data"
+import { useParams } from "react-router-dom";
 
 const Price = (props) => {
+  const {symbol} = useParams() 
+   console.log(symbol);
+
+
+
   // get the data fetched by our loader
-//   const stock = useData();
+  const stock = data.find(element =>{
+    console.log(element);
+    return element.symbol === symbol
+  });
 
   return (
     <div>
       <h1>
-        <h1>{data[0].name}</h1>
-        <h2>{data[0].symbol}</h2>
-        <h2>{data[0].lastPrice}</h2>
-        
+        <h1>{stock.name}</h1>
+        <h2>{stock.symbol}</h2>
+        <h2>{stock.lastPrice}</h2>
+       
       </h1>
       {/* <h2>{stock.open}</h2> */}
     </div>
